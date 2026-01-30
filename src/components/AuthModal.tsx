a17e7906d3a67056afab6ff453ea5b30cbda55ca
jasonschulke <88350049+jasonschulke@users.noreply.github.com>
2026-01-30 09:55:36 -0600

    fixed deploy error

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -192,7 +192,7 @@ export function AuthModal({ isOpen, onClose }: AuthModalProps) {
               {otpCode.map((digit, index) => (
                 <input
                   key={index}
-                  ref={el => inputRefs.current[index] = el}
+                  ref={el => { inputRefs.current[index] = el; }}
                   type="text"
                   inputMode="numeric"
                   maxLength={1}
